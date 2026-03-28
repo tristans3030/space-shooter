0b5162cafdb36f3832bf153e3f19161aa9565fd8
tristans3030 <168497847+tristans3030@users.noreply.github.com>
2026-03-28 00:54:34 -0700

    add files

---
+++ player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var speed := 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = Vector2(100, 500)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	listen_to_input(delta)
+
+# Handle player input for movement
+func listen_to_input(delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	move_and_slide()
