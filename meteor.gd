ae258de2be58a1fa5638d4c749200fe56a570c62
tristans3030 <168497847+tristans3030@users.noreply.github.com>
2026-03-28 13:39:15 -0700

    random meteor direction

--- meteor.gd
+++ meteor.gd
@@ -3,6 +3,7 @@ extends Area2D
 # Meteor speed variables
 var meteor_speed := 400
 var rotation_speed
+var direction_x: float
 @export var max_speed := 700
 @export var min_speed := 300
 
@@ -12,6 +13,7 @@ func _ready() -> void:
 	var rand_speed = rng.randi_range(min_speed, max_speed) # Randomly select a speed for the meteor within the specified range
 	rotation_speed = rng.randf_range(1.0, 5.0) # Set rotation speed based on the meteor speed
 	meteor_speed = rand_speed
+	direction_x = rng.randf_range(-1, 1) # Randomly select a horizontal direction for the meteor
 	if texture == 1:
 		$Sprite2D.texture = preload("res://space_background_pack/Assets/Blue Version/layered/asteroid-1.png")
 	else:
@@ -22,9 +24,10 @@ func _ready() -> void:
 	var random_x = rng.randi_range(0, width)
 	var random_y = rng.randi_range(-150, -50)
 	position = Vector2(random_x, random_y)
+	
 
 func _process(delta: float) -> void:
-	position += Vector2(0, 1.0) * meteor_speed * delta
+	position += Vector2(direction_x, 1.0) * meteor_speed * delta
 	rotation += rotation_speed * delta
 
 func _on_body_entered(body: Node2D) -> void:
