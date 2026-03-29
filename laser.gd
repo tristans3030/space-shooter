d2cbba74c7e9cfdba186b8eb134d112540fc719c
tristans3030 <168497847+tristans3030@users.noreply.github.com>
2026-03-28 18:58:22 -0700

    added animated stars and animated laser

--- laser.gd
+++ laser.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 @export var speed := 500
+@export var anim_speed := 0.25
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Sprite2D.scale = Vector2(0,0)
+	var tween = create_tween()
+	tween.tween_property($Sprite2D, "scale", Vector2(1,1), anim_speed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- level.gd
+++ level.gd
@@ -4,6 +4,23 @@ extends Node2D
 var meteor_scene: PackedScene = load("res://meteor.tscn")
 var laser_scene: PackedScene = load("res://laser.tscn")
 
+func _ready() -> void:
+	# stars
+	var size := get_viewport().get_visible_rect().size
+	var rng := RandomNumberGenerator.new()
+	for star in $Stars.get_children():
+		# position
+		var random_x = rng.randi_range(0, size.x)
+		var random_y = rng.randi_range(0, size.y)
+		star.position = Vector2(random_x, random_y)
+
+		# scale
+		var random_scale = rng.randf_range(-1, -0.5)
+		star.scale = Vector2(random_scale, random_scale)
+
+		# anim speed
+		star.speed_scale = rng.randf_range(0.6, 1.4)
+
 func _on_meteor_timer_timeout() -> void:
 	# instantiate meteor scene
 	var meteor = meteor_scene.instantiate()
