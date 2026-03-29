4fb5bac1aa999c71bf21f1696f5081976c32e3ce
tristans3030 <168497847+tristans3030@users.noreply.github.com>
2026-03-28 23:09:32 -0700

    tweaks

--- game_over.gd
+++ game_over.gd
@@ -6,6 +6,6 @@ func _ready() -> void:
 	$CenterContainer/VBoxContainer/Score.text = "Score: " + str(Global.score)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
 		get_tree().change_scene_to_packed(level_scene)
