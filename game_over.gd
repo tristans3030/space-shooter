0a41c490e9c4b691a43ee06101fa0b50176a9f6a
tristans3030 <168497847+tristans3030@users.noreply.github.com>
2026-03-28 21:28:53 -0700

    added game restart functionality

---
+++ game_over.gd
@@ -0,0 +1,10 @@
+extends Control
+
+var level_scene: PackedScene = load("res://level.tscn")
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("shoot"):
+		get_tree().change_scene_to_packed(level_scene)
